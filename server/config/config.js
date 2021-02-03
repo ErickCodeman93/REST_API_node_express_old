f366872da19fd0666c08c92c89138b11428b4660
Erick93Whiskey <erick@whiskeyindustries.net>
2021-02-03 13:25:05 -0600

    variale de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -19,6 +19,6 @@ let urlDB;
 // if( process.env.NODE_ENV === 'dev' )
 // 	urlDB = 'mongodb://localhost:27017/cafe';
 // else
-urlDB = 'mongodb+srv://striker:c11m6G38hcTboRSB@cluster0.zsna1.mongodb.net/cafe';
+urlDB = process.env.MONGO_URI;
 
 process.env.URLDB = urlDB;
